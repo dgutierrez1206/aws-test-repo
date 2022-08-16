409dbd06b3a4916bc6481f56851a8c61c199c0eb
dgutierrez1206 <dgutierrez@stria.com>
2022-08-16 16:27:35 -0700

    updated cognito config

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-1_sTRcgQWys', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7cf756n4f32emtvvv095i3dovl', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-1.amazonaws.com/prod',
     }
 };
